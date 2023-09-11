77243d45b4332d51d342f5e0d17677f671d15321
Your Name <you@example.com>
2023-09-11 08:56:16 +0300

    mvvmproject

---
+++ app/src/main/java/net/simplifiedcoding/Kourses.kt
@@ -0,0 +1,8 @@
+package net.simplifiedcoding
+
+class Kourses (
+    var KoursesName: String? = "",
+    var KoursesDuration: String? = "",
+    var KoursesDescription: String? = "",
+    var KoursesAuthor: String? = ""
+)
--- app/src/main/java/net/simplifiedcoding/navigation/AppNavHost.kt
+++ app/src/main/java/net/simplifiedcoding/navigation/AppNavHost.kt
@@ -14,13 +14,14 @@ import net.simplifiedcoding.ui.auth.LoginScreen
 import net.simplifiedcoding.ui.auth.SignupScreen
 import net.simplifiedcoding.ui.courses.CourseScreen
 import net.simplifiedcoding.ui.home.HomeScreen
+import net.simplifiedcoding.viewcourse.viewcoursesScreen
 
 @Composable
 fun AppNavHost(
     viewModel: AuthViewModel,
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = ROUTE_LOGIN
+    startDestination: String = ROUTE_VIEWCOURSES
 ) {
     NavHost(
         modifier = modifier,
@@ -41,8 +42,8 @@ fun AppNavHost(
            AddScreen(viewModel, navController)
         }
 
-        composable(ROUTE_VIEW) {
-            CourseScreen(viewModel, navController)
+        composable(ROUTE_VIEWCOURSES) {
+            viewcoursesScreen(viewModel, navController)
         }
 
 
